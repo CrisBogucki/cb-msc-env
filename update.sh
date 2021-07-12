b756862d86dde0c0ffe5069772c5b30cf851b7b1
Krzysztof Bogucki <krzysztof.bogucki@asseco.pl>
2021-07-12 23:41:07 +0200

    .

--- update.sh
+++ update.sh
@@ -14,6 +14,24 @@ PRINT_BANNER() {
 '
 }
 
+DOWNLOAD_CODE(){
+for msc in g-auth g-auth
+do
+  echo "Downloading $msc microservice"
+  
+  rm -rf ./microservices/$msc
+  mkdir ./microservices/$msc
+  cd ./microservices/$msc
+  git clone --recurse-submodules https://github.com/CrisBogucki/cb-msc-$msc.git .
+  clear
+  ./update.sh
+  cd ../..
+done
+clear
+PRINT_BANNER
+echo "Downloading microservices .......... done"
+  
+}
 
 UPDATE_MICROSERVICES() {
 echo "Updating microservices ..............."
@@ -31,4 +49,5 @@ echo "Updating  microservices .......... done"
 }
 
 PRINT_BANNER
-UPDATE_MICROSERVICES
+DOWNLOAD_CODE
+#UPDATE_MICROSERVICES
