2b145f5603ed788cb132fe3548785e8c02c70f24
Krzysztof Bogucki <krzysztof.bogucki@asseco.pl>
2021-07-12 23:57:12 +0200

    .

--- update.sh
+++ update.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 set -eu
 
+path_msc="microservices"
+
 PRINT_BANNER() {
   echo '
   
@@ -14,40 +16,48 @@ PRINT_BANNER() {
 '
 }
 
-DOWNLOAD_CODE(){
-for msc in g-auth g-auth
-do
-  echo "Downloading $msc microservice"
-  
-  rm -rf ./microservices/$msc
-  mkdir ./microservices/$msc
-  cd ./microservices/$msc
-  git clone --recurse-submodules https://github.com/CrisBogucki/cb-msc-$msc.git .
+DOWNLOAD() {
+  for msc in g-auth g-auth; do
+    echo "Downloading $msc microservice"
+
+    rm -rf ./$path_msc/$msc
+    mkdir ./$path_msc/$msc
+    cd ./$path_msc/$msc
+    git clone --recurse-submodules https://github.com/CrisBogucki/cb-msc-$msc.git .
+
+    cd ../..
+    
+  done
   clear
-  ./update.sh
-  cd ../..
-done
-clear
-PRINT_BANNER
-echo "Downloading microservices .......... done"
-  
+  PRINT_BANNER
+  echo "Downloading microservices .......... done"
+
 }
 
-UPDATE_MICROSERVICES() {
-echo "Updating microservices ..............."
-for msc in g-auth g-auth
-do
-  echo "Updating $msc microservice"
-  cd ./microservices/$msc
+UPDATE() {
+  echo "Updating microservices ..............."
+  for msc in g-auth g-auth; do
+    echo "Updating $msc microservice"
+    cd ./$path_msc/$msc
+    clear
+    ./update.sh
+    cd ../..
+  done
   clear
-  ./update.sh
-  cd ../..
-done
-clear
-PRINT_BANNER
-echo "Updating  microservices .......... done"
+  PRINT_BANNER
+  echo "Updating  microservices .......... done"
+}
+
+DEPLOY_BROCKER(){
+sh ./deploy-brocker.sh
+}
+
+DEPLOY_MICROSERVICES() {
+sh ./deploy-microservices.sh
 }
 
 PRINT_BANNER
-DOWNLOAD_CODE
-#UPDATE_MICROSERVICES
+DOWNLOAD
+UPDATE
+DEPLOY_BROCKER
+DEPLOY_MICROSERVICES
